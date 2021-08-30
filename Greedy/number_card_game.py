df2fc8a51b46e381e91eaf0a7d212deb9ab874cf
jiwon <zest1923@gmail.com>
2021-08-30 20:25:46 +0900

    update

--- Greedy/number_card_game.py
+++ Greedy/number_card_game.py
@@ -1,11 +1,8 @@
-row, column = map(int, input().split())
+n, m = map(int, input().split())
+value = 0
 
-# 2차원 배열 입력받기
-cards = [list(map(int, input().split())) for _ in range(column) ]
-
-#각 행에 존재하는 제일 작은 값 넣기
-mincard = [] 
-for i in range(column):
-  mincard.append(min(cards[i]))
-# 그 중에 제일 큰 카드 값 찾기
-print(max(mincard))
+for i in range(n):
+    data = list(map(int, input().split()))
+    if value < min(data):
+        value = min(data)
+print(data)
