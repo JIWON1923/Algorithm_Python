cf59baacb5b5e548cc266d559202059b4af11411
jiwon <zest1923@gmail.com>
2021-08-30 20:25:32 +0900

    update

--- Greedy/law_of_large_number.py
+++ Greedy/law_of_large_number.py
@@ -2,9 +2,8 @@ n, m, k = map(int, input().split())
 num_list = list(map(int, input().split()))
 
 num_list.sort (reverse=True)
-result = 0
 
-for i in range(m):
-  if k%(i+1) != 0: result += num_list[0]
-  else: result += num_list[1]
+count = m//(k+1)
+result = count * num_list[1]
+result += (m-count) * num_list[0]
 print(result)
