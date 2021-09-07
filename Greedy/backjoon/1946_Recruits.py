a7683c8b95e31735d0de11e7fff4b72d22780756
jiwon <zest1923@gmail.com>
2021-09-07 11:35:07 +0900

    timeout problem solved. (using readline)

--- Greedy/backjoon/1946_Recruits.py
+++ Greedy/backjoon/1946_Recruits.py
@@ -1,16 +1,13 @@
+import sys
 case = int(input())
-result = []
-for i in range(case):
-    score = []
-    cnt = 1
+for _ in range(case):
     applicant = int(input())
-    for i in range(applicant):
-        score.append(list(map(int, input().split())))
-    score = sorted(score, key=lambda x: x[0])
-    high_score = score[0][1]
-    for j in range(applicant):
-        if high_score > score[j][1]: # 높은 등
-            cnt += 1
-            high_score = score[j][1]
-    result.append(cnt)
-print(" ".join(repr(i)for i in result))
+    score = sorted([list(map(int, sys.stdin.readline().strip().split())) for x in range(applicant)], key=lambda x:x[0])
+    result = 1
+    high_score = score[0][1] # = 작은 숫자의 등수(높은 점수)
+
+    for i in range(1, applicant):
+        if high_score > score[i][1]: # 점수가 더 높으면
+            high_score = score[i][1]
+            result += 1
+    print(result)
