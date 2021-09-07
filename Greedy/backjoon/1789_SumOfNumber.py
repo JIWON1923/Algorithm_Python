e904885bdcc417f3ee47a6a939823ba1e5b48901
jiwon <zest1923@gmail.com>
2021-09-07 19:40:41 +0900

    using for loop and if statement

--- Greedy/backjoon/1789_SumOfNumber.py
+++ Greedy/backjoon/1789_SumOfNumber.py
@@ -1,12 +1,11 @@
 num = int(input())
-temp = 0
-result = 0
+result, cnt = 0, 0
 
-for i in range(2, num):
-    if temp > num: break
-    temp += i
-    result += 1
+for i in range(1, num): # 0, 1, 2, 3, 4
+    cnt += 1
+    result += i
+    if result >= num : break
 
-if num <= 2: result = 1
-if num == 3: result = 2
-print(result)
+if result == num : print(cnt)
+elif result > num : print(cnt-1)
+elif result < 3 : print(1)
