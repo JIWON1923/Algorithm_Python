823e197d510eca21e4f02aaa179a34267262182c
jiwon <zest1923@gmail.com>
2021-09-07 21:40:44 +0900

    using set

---
+++ BasicLevel/Class1/1157_WordCount.py
@@ -0,0 +1,13 @@
+alphabet = input().upper()
+list(alphabet).sort()
+set_alphabet = set(alphabet)
+max_count = 0
+index = 0
+
+for alpha in set_alphabet:
+    now = alphabet.count(alpha)
+    if max_count < now:
+        max_alpha = alpha
+        max_count = now
+    elif max_count == now: max_alpha = '?'
+print(max_alpha)
