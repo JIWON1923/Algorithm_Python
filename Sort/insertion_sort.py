7286c4fa0c9a5920fd12fefd381ca4165d7c0bfc
jiwon <zest1923@gmail.com>
2021-11-12 18:09:19 +0900

    insertion_sort

---
+++ Sort/insertion_sort.py
@@ -0,0 +1,8 @@
+arr = [9, 2, 7, 4, 3, 8, 1]
+
+for i in range(len(arr)):
+    for j in range(i, 0, -1):
+        if arr[j] < arr[j-1]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+        else: break
+print(arr)
