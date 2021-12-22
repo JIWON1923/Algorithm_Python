68170ffc3fd85f854fef8611fa07e8bbc705c95b
jiwon <zest1923@gmail.com>
2021-12-22 23:54:57 +0900

    없는 숫자 더하기

--- Programmers/Level1/sumOfCertainNumber.py
+++ Programmers/Level1/sumOfCertainNumber.py
@@ -1,5 +1,3 @@
 def solution(numbers):
     basic = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-    numbers = set(numbers)
-    result = basic - numbers
-    return sum(result)
+    return sum(basic - set(numbers))
