77eb8747ce9ece9b9ce956a3886842b40c37f4f8
jiwon <zest1923@gmail.com>
2022-01-14 00:54:21 +0900

    [1차] 비밀지도

--- Programmers/Level1/secretMap.py
+++ Programmers/Level1/secretMap.py
@@ -2,8 +2,6 @@ def solution(n, arr1, arr2):
     answer = []
     for i in range(n):
         answer.append(arr1[i] | arr2[i])
-        answer[i] = "0" * (n - (len(bin(answer[i])) - 2)) + str(bin(answer[i]))[2:]
-        answer[i] = answer[i].replace('1', '#')
-        answer[i] = answer[i].replace('0', ' ')
-
+        answer[i] = "0" * (n - (len(bin(answer[i]))-2)) + str(bin(answer[i]))[2:]
+        answer[i] = answer[i].replace('1', '#').replace('0', ' ')
     return answer
