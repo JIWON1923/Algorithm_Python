be82430cf11daca6a41eabdef9c6c88af90755e6
jiwon <zest1923@gmail.com>
2022-02-14 01:38:19 +0900

    나무자르기, python 시간초과 해결

--- BasicLevel/Class2/2805_cuttingTheTree.py
+++ BasicLevel/Class2/2805_cuttingTheTree.py
@@ -4,11 +4,8 @@ trees = list(map(int, sys.stdin.readline().split()))
 start, end = 0, max(trees)
 
 while start <= end:
-    total = 0
     mid = (start + end) // 2
-    for i in trees:
-        if i > mid:
-            total += (i-mid)
+    total = sum([i-mid for i in trees if i > mid])
     if total < m:
         end = mid-1
     else:
