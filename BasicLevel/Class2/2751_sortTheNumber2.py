81cd3e45e3e5f6b6c016d5a3861c2ce9413e317a
jiwon <zest1923@gmail.com>
2022-02-15 02:11:56 +0900

    수 정렬하기2 - 기본 sort함수 사용

--- BasicLevel/Class2/2751_sortTheNumber2.py
+++ BasicLevel/Class2/2751_sortTheNumber2.py
@@ -1,6 +1,7 @@
 #시간초과
+import sys
 n = int(input())
 numbers = []
 for i in range(n):
-    numbers.append(int(input()))
+    numbers.append(int(sys.stdin.readline()))
 print('\n'.join(repr(i) for i in sorted(numbers)))
