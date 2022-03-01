e1b0f09b1737c4ec9160e9dd2e6d54edd8a03970
jiwon <zest1923@gmail.com>
2022-03-01 21:26:37 +0900

    균형잡힌세상, 틀렸습니다....

---
+++ BasicLevel/Class2/4949_balancedWorld.py
@@ -0,0 +1,23 @@
+while 1:
+    check = input()
+    if check == '.': break
+    stack = []
+    result = True
+    for i in check:
+        if i == '[' or i == '(':
+            stack.append(i)
+        if i == ']' or i == ')':
+            if len(stack) == 0:
+                result = False
+                break
+            c = stack.pop(-1)
+            if i == ']' and c != '[':
+                result = False
+                break
+            if i == ')' and c == '(':
+                result = False
+                break
+    if result and not len(stack):
+        print('yes')
+    else:
+        print('no')
